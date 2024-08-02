19deb3a6350cfaedd598ef4f58785951b971898d
Dinesh <dineshdesu2518@gmail.com>
2024-08-02 19:55:07 +0530

    corrected index.js

--- index.js
+++ index.js
@@ -3,21 +3,9 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://your-production-frontend.com",
-];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
