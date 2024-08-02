b2d343a3db1206fcb5b6a1c0a465d1b34258dfdc
Dinesh <dineshdesu2518@gmail.com>
2024-08-02 19:45:35 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://your-production-frontend.com",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
